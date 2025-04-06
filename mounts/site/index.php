dc4a3c8ea7b1521abf101d9de3430799ca5f95cb
Shvaapsss <145658659+Shvaapsss@users.noreply.github.com>
2025-04-06 15:51:09 +0300

    Create index.php

---
+++ mounts/site/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Главная страница
+ */
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Добро пожаловать</title>
+</head>
+<body>
+    <h2>Добро пожаловать в тест!</h2>
+    <p>Пройдите тест и проверьте свои знания о яблоках.</p>
+    <a href="test.php"><button>Начать тест</button></a>
+</body>
+</html>
